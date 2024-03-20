2aa56b785540a8842dd72cba1de7f369d8d57a07
Muflih Raihan <muflihraihan693@gmail.com>
2024-03-20 23:48:08 +0700

    migration database

---
+++ database/migrations/2024_03_20_073212_create_profile_sekolah.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('profile_sekolah', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_sekolah');
+            $table->string('alamat');
+            $table->string('nomor_telepon');
+            $table->string('email');
+            $table->string('kode_pos');
+            $table->string('deskripsi_sejarah');
+            $table->string('visi_misi');
+            $table->string('motto');
+            $table->string('sambuatan_kepsek');
+            $table->string('tujuan_sekolah');
+            $table->string('logo_sekolah');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('profile_sekolah');
+    }
+};
--- database/migrations/2024_03_20_073518_create_mata_pelajaran.php
+++ database/migrations/2024_03_20_073518_create_mata_pelajaran.php
@@ -11,13 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('mata_pelajaran', function (Blueprint $table) {
             $table->id();
-            $table->string('username');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('role');
-            $table->rememberToken();
+            $table->string('nama_matapelajaran');
             $table->timestamps();
         });
     }
@@ -27,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('mata_pelajaran');
     }
 };
