10b83df235c620c88d149f3502e5b929410028e2
miftahsabirah <128968443+miftahsabirah@users.noreply.github.com>
2024-06-22 18:35:25 +0700

    Revert "Miftah (#14)" (#17)
    This reverts commit 51899c53757bd3fb4fe2c40dd070c5d62ba6a00e.

--- routes/web.php
+++ routes/web.php
@@ -20,64 +20,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     // return view('home');
-
-    return view('admin/guruKaryawan');
-});
-
-// Route::get('/', function () {
-//     // return view('home');
-//     return view('login');
-// });
-Route::get('/home', function () {
-    // return view('home');
     return view('home');
 });
-
-Route::get('/home', function () {
-    return view('home');
-})->name('home');
-
-Route::get('/login', function () {
-    return view('login');
-})->name('login');
-
-Route::get('/sambutan', function () {
-    return view('profil/sambutan');
-})->name('sambutan');
-
-Route::get('/sejarah', function () {
-    return view('profil/sejarah');
-})->name('sejarah');
-
-Route::get('/visiMisi', function () {
-    return view('profil/visiMisi');
-})->name('visiMisi');
-
-Route::get('/guruKaryawan', function () {
-    return view('profil/guruKaryawan');
-})->name('guruKaryawan');
-
-Route::get('/akreditasi', function () {
-    return view('profil/listAkreditasi');
-})->name('akreditasi');
-
-
-
-Route::get('/kalenderAkademik', function () {
-    return view('akademik/kalenderAkademik');
-})->name('kalenderAkademik');
-
-Route::get('/detailBerita', function () {
-    return view('informasi/detailBerita');
-})->name('detailBerita');
-
-Route::get('/sarpras', function () {
-    return view('sarpras/sarpras');
-})->name('sarpras');
-
-Route::get('/detailBerita', function () {
-    return view('informasi/detailBerita');
-})->name('detailBerita');
 
 
 
